cbead0e961b3e149370c262579fcc63394fd0ca6
Aditya <aditya.sasmal.dev@gmail.com>
2024-06-17 10:22:49 +0530

    Adds the transactions page
    Builds the transaction page.
    
    Displays all transaction details and also alllows users to edit the
    categories and accounts.Uncategorized transactions can be given a
    category.
    
    Implements editing transactions,also allows users to bulk delete
    transactions.
    
    Displays the formated amount using seperate colors to indicate
    income/expense.

---
+++ src/app/(dashboard)/transactions/page.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import { Loader2, Plus } from "lucide-react";
+
+import { useGetTransactions } from "@/features/transactions/api/use-get-transactions";
+import { useNewTransaction } from "@/features/transactions/hooks/use-new-transaction";
+import { useBulkDeleteTransactions } from "@/features/transactions/api/use-bulk-delete-transactions";
+
+import { Button } from "@/components/ui/button";
+import { DataTable } from "@/components/data-table";
+import { Skeleton } from "@/components/ui/skeleton";
+
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
+import { columns } from "./columns";
+
+const TransactionsPage = () => {
+  const { onOpen } = useNewTransaction();
+  const transactionsQuery = useGetTransactions();
+  const deleteTransactions = useBulkDeleteTransactions();
+
+  const isDisabled =
+    transactionsQuery.isLoading || deleteTransactions.isPending;
+
+  const transactions = transactionsQuery.data || [];
+
+  if (transactionsQuery.isLoading) {
+    return (
+      <div className="max-w-screen-2xl mx-auto -mt-24 pb-10">
+        <Card className="border-none drop-shadow-sm">
+          <CardHeader>
+            <Skeleton className="h-8 w-48" />
+          </CardHeader>
+          <CardContent>
+            <div className="h-[500px] w-full flex items-center justify-center">
+              <Loader2 className="size-6 text-slate-300 animate-spin" />
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    );
+  }
+
+  return (
+    <div className="max-w-screen-2xl mx-auto -mt-24 pb-10">
+      <Card className="border-none drop-shadow-sm">
+        <CardHeader className="gap-y-2 lg:flex-row lg:justify-between lg:items-center">
+          <CardTitle className="text-xl line-clamp-1">
+            Transactions History
+          </CardTitle>
+          <Button onClick={onOpen}>
+            <Plus />
+            Add new
+          </Button>
+        </CardHeader>
+        <CardContent>
+          <DataTable
+            onDelete={(rows) => {
+              const ids = rows.map((r) => r.original.id);
+              deleteTransactions.mutate({ ids });
+            }}
+            disabled={isDisabled}
+            filterKey="payee"
+            columns={columns}
+            data={transactions}
+          />
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default TransactionsPage;
