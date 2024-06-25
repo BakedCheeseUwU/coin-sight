0cb8799539769549800694bd51aa5d129a3e584e
Aditya <aditya.sasmal.dev@gmail.com>
2024-06-25 20:53:27 +0530

    feat: loading states for info cards

--- src/components/data-card.tsx
+++ src/components/data-card.tsx
@@ -4,6 +4,15 @@ import { IconType } from "react-icons/lib";
 import { cn, formatCurrency, formatPercentage } from "@/lib/utils";
 
 import { CountUp } from "@/components/count-up";
+import { Skeleton } from "./ui/skeleton";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardTitle,
+  CardHeader,
+} from "@/components/ui/card";
+
 const boxVariant = cva("shrink-0 rounded-md p-3", {
   variants: {
     variant: {
@@ -84,6 +93,26 @@ export const DataCard = ({
         >
           {formatPercentage(percentageChange)} from last period
         </p>
+      </CardContent>
+    </Card>
+  );
+};
+
+export const DataCardLoading = () => {
+  return (
+    <Card className="h-[192px] border-none drop-shadow-sm">
+      <CardHeader className="flex flex-row items-center justify-between">
+        <div className="space-y-2">
+          <Skeleton className="h-6 w-24" />
+          <Skeleton className="h-6 w-40" />
+        </div>
+
+        <Skeleton className="size-12" />
+      </CardHeader>
+
+      <CardContent>
+        <Skeleton className="mb-2 h-10 w-24 shrink-0" />
+        <Skeleton className="h-4 w-40 shrink-0" />
       </CardContent>
     </Card>
   );
